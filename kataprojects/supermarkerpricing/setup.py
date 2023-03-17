c519d941b6e57dea4784535153571f93170fb578
kareemIOI <kareemiisafar@gmail.com>
2023-03-17 23:27:19 +0300

    Structuring the logic

--- kataprojects/supermarkerpricing/setup.py
+++ kataprojects/supermarkerpricing/setup.py
@@ -1,11 +1,36 @@
+import sys
+import time
+context = {}
+SeriesOfInputs = []
+
 
 
 def main():
-    with open("Sales Report.csv") as handler:  
-        data = handler.read()
-        print(data)
-        #TODO DATA IS BEING READ
+    userInput = input("Please Follow the Instructions Given (y) to continue (q) to break: ")
+    if userInput == 'y':
+        products()
+    else:
+        RageQuit()
+        
+        
+def products():
+    productsInfo = input('These are the available products that we have.. kindly provide us with your needs: ')
+    
 
 
-if __name__ == '__main__':
+def RageQuit():
+    print('Data has been saved')
+    time.sleep(1)
+    print('exiting in')
+    print('3')
+    time.sleep(1)
+    print('2')
+    time.sleep(1)
+    print('1')
+    time.sleep(1)
+    sys.exit()
+
+
+
+while True:
     main()
